333a8015afcc397bce751e2b40b043e85330b544
prajhub <pzlmagar@gmail.com>
2023-06-03 17:04:33 +0545

    Updated the projects section.

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import NavBar from '../Components/NavBar'
+import Work from '../Components/Work'
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
     
         <NavBar/>
         <Hero/>
+        <Work/>
     </>
   )
 }
