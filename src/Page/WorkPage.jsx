9fb7a6efc5282f742427b82989799aa42d75a98c
prajhub <pzlmagar@gmail.com>
2023-06-04 20:45:09 +0545

    Finished my works page.

--- src/Page/WorkPage.jsx
+++ src/Page/WorkPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import NavBar from '../Components/NavBar'
 import WorkHero from '../Components/WorkComponents/WorkHero'
 import Projects from '../Components/WorkComponents/Projects'
+import Socials from '../Components/Socials'
+import Footer from '../Components/Footer'
 
 const WorkPage = () => {
   return (
@@ -10,6 +12,8 @@ const WorkPage = () => {
         <NavBar/>
         <WorkHero/>
         <Projects/>
+        <Socials/>
+        <Footer/>
    
    </>
   )
